7663072a7657b2998a4b2cbb9f4de5d14e8f0674
araa <arguello.ramosadrian@gmail.com>
2022-12-16 00:43:21 -0800

    Modifique navbar, agrege navbar.js y navbar.css para acomodar bien los archivos

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+let searchButton = document.getElementById("searchButton");
+let searchInput = document.getElementById("searchInput");
+let cancelSearch = document.getElementById("cancelSearch");
+
+searchButton.addEventListener("click",function(){
+    cancelSearch.style.display = "block";
+    searchInput.style.display = "block";
+    searchButton.style.display = "none";
+})
+
+cancelSearch.addEventListener("click",function(){
+    cancelSearch.style.display = "none";
+    searchInput.style.display = "none";
+    searchInput.value = "";
+    searchButton.style.display = "block";
+})
