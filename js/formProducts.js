64e98f2d7e4c528ddc93ec0864158d98884093fc
araa <arguello.ramosadrian@gmail.com>
2023-01-06 17:37:19 -0800

    Se agrego localstorage para los productos en el index.html

--- js/formProducts.js
+++ js/formProducts.js
@@ -1,26 +1,19 @@
 let form = document.getElementById("form");
 let alertError2 = document.getElementById("alertError2");
 
+
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   alertError2.innerHTML = "";
   alertError2.style.display="none";
   const data = new FormData(e.target);
-  const value = Object.fromEntries(data.entries());
+  const product = Object.fromEntries(data.entries());
 
- 
-
-  console.log(value); // Borrar en futuras versiones
-  //console.log(value.name);
-
-  value.name
-  value.price
-  value.description
-  value.stock
   let valido = true;
   //console.log(parseInt(value.price))
 
-  if (value.name.length < 3 || value.name.length > 20)
+  if (product.name.length < 3 || product.name.length > 20)
   {
     valido = false;
     alertError2.style.display="block";
@@ -28,28 +21,31 @@ form.addEventListener("submit", (e) => {
   }
 
   
-  if (parseInt(value.price)<=0 || isNaN(parseInt(value.price)))
+  if (parseInt(product.price)<=0 || isNaN(parseInt(product.price)))
   {
     valido = false;
     alertError2.style.display="block";
     alertError2.innerHTML += "<br/> Ingresa un precio valido";
   }
 
-  if (value.description.length < 15)
+  if (product.description.length < 15)
   {
     valido = false;
     alertError2.style.display="block";
     alertError2.innerHTML += "<br/> La descripcion debe contener mas de 15 caracteres";
   }
 
-  if (parseInt(value.stock) < 0 || isNaN(parseInt(value.stock)) )
+  if (parseInt(product.stock) < 0 || isNaN(parseInt(product.stock)) )
   {
     valido = false;
     alertError2.style.display="block";
     alertError2.innerHTML += "<br/>Ingresa productos disponibles";
   }
- 
 
+
+  if(valido){
+    localStorage.setItem("product", JSON.stringify(product));
+  }
 })
 
 
--- js/main.js
+++ js/main.js
@@ -8,19 +8,21 @@ function getAllProducts(){
 }
 
 function addItem(product){
-    const itemHTML = '<div class="card" style="width: 18rem;">\n' +
-        '    <img src="'+product.img +'" class="card-img-top" alt="image">\n' +
-        '    <div class="card-body">\n' +
-        '        <h5 class="card-title">'+product.name+'</h5>\n' +
-        '        <p class="card-text">'+product.description+'</p>\n' +
-        '        <a href="#" class="btn btn-light" type="submit" style="background-color: #EFCFCF;">Agregar al carrito</a>\n' +
-        '    </div>\n' +
-        '</div>\n' +
-        '<br/>';
+    const itemHTML = `<div class="card" style="width: 18rem;">
+            <img src="${product.file}" style="width:120px; height:120px;"class="card-img-top" alt="image">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">${product.description}</p>
+                <a href="#" class="btn btn-light" type="submit" style="background-color: #EFCFCF;">Agregar al carrito</a>
+            </div>
+        </div>
+        <br/>`;
     const itemsContainer = document.getElementById("list-items");
     itemsContainer.innerHTML += itemHTML;
 }
 
 window.addEventListener("load", () => {
     getAllProducts();
+    let product = JSON.parse(localStorage.getItem("product"));
+    addItem(product);
   });
