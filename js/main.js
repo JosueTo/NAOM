9d4d8be8706b3b2a12f1885666ca8dfdf1f12fd7
Josue Torres <josuetolvera@gmail.com>
2023-01-08 23:45:36 -0600

    Añadir efecto carrusel a los productos

--- js/main.js
+++ js/main.js
@@ -1,28 +1,64 @@
-function getAllProducts(){
-     fetch('../js/products.json')
-     .then(response => response.json()).then(data => {
-        data.products.forEach(product => {
-            addItem(product);
-        });
-     })
+function getAllProducts() {
+    fetch('../js/products.json')
+        .then(response => response.json()).then(data => {
+            data.products.forEach(product => {
+                addItem(product);
+            });
+        })
 }
 
-function addItem(product){
-    const itemHTML = `<div class="card" style="width: 18rem;">
-            <img src="${product.file}" style="width:120px; height:120px;"class="card-img-top" alt="image">
-            <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <p class="card-text">${product.description}</p>
-                <a href="#" class="btn btn-light" type="submit" style="background-color: #EFCFCF;">Agregar al carrito</a>
-            </div>
-        </div>
-        <br/>`;
+function addItem(product) {
+    const itemHTML = 
+    `
+    <div class="swiper-slide">
+    <img src="${product.file}" "class="card-img-top" alt="image">
+    <div class="card-body">
+        <h5 class="card-title">${product.name}</h5>
+        <p class="card-text">${product.description}</p>
+        <a href="#" class="btn btn-light" type="submit" style="background-color: #EFCFCF;">Agregar al carrito</a>
+    </div>
+    </div>
+    `;
+
     const itemsContainer = document.getElementById("list-items");
     itemsContainer.innerHTML += itemHTML;
 }
+
+
 
 window.addEventListener("load", () => {
     getAllProducts();
     let product = JSON.parse(localStorage.getItem("product"));
     addItem(product);
-  });
+});
+
+
+// Carousel
+let swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    breakpoints: {
+        600: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        680: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        920: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+        },
+        1240: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+
+    }
+});
